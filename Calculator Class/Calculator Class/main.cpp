2eef46e1d8641340f70fbd45de5b061b566282c1
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-03-05 12:14:33 +0100

    Added calculator class on xcode

---
+++ Calculator Class/Calculator Class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Calculator Class
+//
+//  Created by Unnie Jansson on 2024-03-05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
