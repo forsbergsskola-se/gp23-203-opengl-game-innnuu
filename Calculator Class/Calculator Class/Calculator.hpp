8cc8d492350063c3dcc569e3c09852c382ef32a3
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-03-05 15:31:00 +0100

    not sure it this code should be in hpp or cpp file

--- Calculator Class/Calculator Class/Calculator.hpp
+++ Calculator Class/Calculator Class/Calculator.hpp
@@ -1,46 +1,31 @@
 
-#ifndef Calculator_hpp
-#define Calculator_hpp
-
-#include <stdio.h>
-#endif /* Calculator_hpp */
-
-
+#include <iostream>
+using namespace std;
 enum class Operations{
-    
     add,
     Subtract,
     Multiply,
     Divide,
     None
 };
-
-
 class Calculator{
-
 private:
     double number;
     Operations currentOperation;
-    
 public:
     Calculator() : number(0), currentOperation(Operations::None){}
-    
     void SetNumber(double num){
-        
         switch (currentOperation)
         {
             case Operations::None:
                 number = num;
                 break;
-                
             case Operations::add:
                 number +=num;
                 break;
-                
             case Operations::Subtract:
                 number -= num;
                 break;
-                
             case Operations::Multiply:
                 number *= num;
                 break;
@@ -48,10 +33,37 @@ public:
             case Operations::Divide:
                 number /=num;
                 break;
-                
-                
+                default:
+                break;
         }
     }
-
+    void SetOperation (Operations op){
+        currentOperation = op; //setting the current operation
+    }
+    double getNumber(){ //to get the correct number after the operation
+        return number;}
+    void clear(){
+        number = 0; //so we reset the number and can start again after.
+        currentOperation = Operations::None; //this resets the operation
+    }
+  
     
+    
+};
+
+int main(){
+    
+    Calculator calculator;
+    calculator.SetNumber(5);
+    calculator.SetOperation(Operations::add);
+    calculator.SetNumber(3); //this should be 8
+    printf("%f",calculator.getNumber());
+    calculator.clear(); // Reset calculator
+    calculator.SetNumber(2); // Set new initial number
+    calculator.SetOperation(Operations::Multiply); // Set operation to Multiply
+    calculator.SetNumber(3); // Perform multiplication
+    calculator.SetOperation(Operations::Subtract); // Set operation to Subtract
+    calculator.SetNumber(1); // Perform subtraction, should output 5
+    printf("%f",calculator.getNumber());
+    return 0;
 };
